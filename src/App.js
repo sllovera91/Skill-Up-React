92fed1cf696e29a27f2205f79d50a75ba626e976
Christian Daniel Ortiz Ororbia <cortiz2894@gmail.com>
2022-10-11 12:51:31 -0300

    Create app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'./alkemy_logo.svg'} className="App-logo" alt="logo" />
+        <p>
+          Bienvenido a AlkyBank
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
