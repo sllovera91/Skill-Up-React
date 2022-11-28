fd217008b7116cdf812c872608e49bf57ee12c82
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-11-28 19:58:58 -0300

    add: format and .gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'./alkemy_logo.svg'} className="App-logo" alt="logo" />
+        <img src={"./alkemy_logo.svg"} className="App-logo" alt="logo" />
         <p>
           Bienvenido a AlkyBank
         </p>
