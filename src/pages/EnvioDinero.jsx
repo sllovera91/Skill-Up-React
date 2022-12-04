8c84ed368117afc7fdb0ed87dfc9970aa6aaf43c
Marianopiv <marianopividori93@gmail.com>
2022-12-04 12:46:08 -0300

    aaa

--- src/pages/EnvioDinero.jsx
+++ src/pages/EnvioDinero.jsx
@@ -6,7 +6,7 @@ export const EnvioDinero = () => {
     <div className=" d-flex flex-column justify-content-center my-5 align-items-center">
       <Title size={"h1"}>Transferí dinero a tus amigos</Title>
       <div className="d-flex row py-5 my-4 col-12">
-        <div className="d-flex flex-column align-items-center my-5 col-6 justify-content-center">
+        <div className="d-flex  flex-column align-items-center my-5 col-6 justify-content-center">
           <Title size={"h4"}>Transferir por ID de cuenta</Title>
           <label className="text-center" htmlFor="">ID de la cuenta</label>
           <input className=" my-4 border border-1 border-secondary opacity-50  rounded-1 " type="number" name="" id="" />
