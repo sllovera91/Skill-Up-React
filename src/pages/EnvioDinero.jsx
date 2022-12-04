2401625c729f94b06851b2afc75c9c7f2e7fe022
Marianopiv <marianopividori93@gmail.com>
2022-12-04 12:41:02 -0300

    mejoro los borders

--- src/pages/EnvioDinero.jsx
+++ src/pages/EnvioDinero.jsx
@@ -3,8 +3,24 @@ import { Title } from "../components/Title";
 
 export const EnvioDinero = () => {
   return (
-    <div className=" d-flex justify-content-center">
+    <div className=" d-flex flex-column justify-content-center my-5 align-items-center">
       <Title size={"h1"}>Transferí dinero a tus amigos</Title>
+      <div className="d-flex row py-5 my-4 col-12">
+        <div className="d-flex flex-column align-items-center my-5 col-6 justify-content-center">
+          <Title size={"h4"}>Transferir por ID de cuenta</Title>
+          <label className="text-center" htmlFor="">ID de la cuenta</label>
+          <input className=" my-4 border border-1 border-secondary opacity-50  rounded-1 " type="number" name="" id="" />
+          <label className="text-center" htmlFor="">importe a transferir</label>
+          <input className=" my-4 border border-1 border-secondary opacity-50  rounded-1 " type="number" name="" id="" />
+        </div>
+        <div className="d-flex flex-column align-items-center my-5 col-6 justify-content-center">
+          <Title size={"h4"}>Transferir con email</Title>
+          <label className="text-center" htmlFor="">Mail del usuario</label>
+          <input className=" my-4 border border-1 border-secondary opacity-50  rounded-1 " type="number" name="" id="" />
+          <label className="text-center" htmlFor="">importe a transferir</label>
+          <input className=" my-4 border border-1 border-secondary opacity-50  rounded-1 " type="number" name="" id="" />
+        </div>
+      </div>
     </div>
   );
 };
