a00cbddcc9a07d693754c4c6f85fcf1d1d47ca0a
Santiago <sllovera91@hotmail.com>
2022-12-05 00:14:23 -0300

    autenticacion terminada, se añadale al navbar el salir

---
+++ src/redux/slices/auth.Slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    status: 'validando',
+    user: {},
+    errorMsg: undefined,
+  },
+  reducers: {
+    onChecking: (state) => {
+      state.status = 'validando'
+      state.user = {};
+      state.errorMsg = undefined 
+    },
+    onLogin: ( state, action) => {
+      state.status = 'validado'
+      state.user = action.payload
+      state.errorMsg = undefined
+    },
+    onLogout: ( state, action ) => {
+        state.status = 'no-validado';
+        state.user   = {};
+        state.errorMsg = action.payload;
+  }
+    }
+});
+
+export const { onChecking, onLogin, onLogout } = authSlice.actions;
+
--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -2,11 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login/Login";
 import { WithNavBar } from "./layout/WithNavBar";
 import { Inicio, Balance, CargaSaldo, EnvioDinero, Gastos, Movimientos, Error404 } from "../pages";
+import { useEffect } from "react";
+import { useAuth } from "../hooks/useAuth";
+
+
 
 export const Rutas = () => {
-  const status = "no-validado";
+
+  const { status, verifToken } = useAuth();
+
+  useEffect(() => {
+    verifToken();
+  }, [])
+
+  if ( status === 'validando' ) {
+    return (
+      <h1>Aguarde un momento...</h1>
+    )
+  }
 
   return (
+
   <BrowserRouter>
     <Routes>
     {
