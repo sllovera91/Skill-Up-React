adefec05a03d0bb9ce15cec3a2136d813114446e
Marianopiv <marianopividori93@gmail.com>
2022-12-05 19:41:55 -0300

    traigo con get movimientos y agrego UI de cargar saldo

--- src/pages/Movimientos.jsx
+++ src/pages/Movimientos.jsx
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import alkemyApi from "../api/login.js";
 import { Transaction } from "../components/Transaction.jsx";
 
 export const Movimientos = () => {
-  const [operations] = useState(["operation"]);
+  const [operations, setOperations] = useState([]);
 
+  const token = localStorage.getItem("token");
+
+  const Autorizacion = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+
+   const get = async () => {
+    try {
+      const response = await alkemyApi.get("/transactions", Autorizacion);
+      console.log(response);
+      setOperations(response.data.data);
+    } catch (error) {
+      console.log("no anduvo");
+    }
+  };
+
+  useEffect(() => {
+    get();
+  }, []);
+  /* "{"concept":"Quick charge","currencyCode":"ARS","isTransference":false} */
   return (
     <div className="Content">
-      <div className="container shadow p-3 my-auto bg-white rounded m-10">
+      <div className="container shadow p-3 my-auto bg-white rounded m-8 px-5">
         {operations.length !== 0
           ? <>
 
@@ -16,24 +39,19 @@ export const Movimientos = () => {
                   <th scope="col">Concepto</th>
                   <th scope="col">Importe</th>
                   <th scope="col">
-                    <select className="form-select text-dark" aria-label="Default select example">
+                    <select className="form-select text-dark w-75" aria-label="Default select example">
                       <option selected>Ingreso/Egreso</option>
                       <option value="1">Ingreso</option>
                       <option value="2">Egreso</option>
                     </select></th>
-                  <th scope="col">Fecha</th>
+                  <th scope="col-3">Fecha</th>
                 </tr>
               </thead>
               <tbody>
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
-                <Transaction />
+                <div className="d-flex flex-column flex-wrap">{operations.length > 0 && operations.map(({ concept, amount, date, type }, index) => (
+                <div className="" key={index}><Transaction concept={concept} amount={amount} date={date} type={type} />
+                </div>
+                ))}</div>
               </tbody>
             </table>
               <nav>
