ba902b9078c44943654c003842681a003168691c
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-12-06 19:00:43 -0300

    fix vh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="text-center text-white bg-primary">
+    <footer className="footer text-center text-white bg-primary">
       <div className="text-center p-4">
         2022 - Grupo Alkemy
       </div>
