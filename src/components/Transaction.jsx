a2397913a8a252d2c4a9592e4063b3df39ac1df4
Marianopiv <marianopividori93@gmail.com>
2022-12-06 22:35:07 -0300

    detalle etiquetas arreglado

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -3,12 +3,12 @@ import { handleDate } from "../helper/helper";
 
 export const Transaction = ({ concept, amount, date, type }) => {
   return (
-
+    <tr>
     <div className="d-flex justify-content-between">
       <div className="col-12">
         <td className="text-secondary">{concept}</td>
       </div>
-    </td>
+    </div>
     <td>
       <p className="text-secondary">${amount}</p>
       </td>
