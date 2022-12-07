318fdf9d16570511ac09e12c5d041f7a74758898
Marianopiv <marianopividori93@gmail.com>
2022-12-06 23:57:26 -0300

    aaa

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Button } from "../components/Button";
 import { Title } from "../components/Title";
 import { Link } from "react-router-dom";
-/* import "animate.css";
- */
+import "animate.css";
+
   export const Inicio = () => {
   return (
-    <div className="Content text-center d-flex align-items-center justify-content-center mt-5 animate__animated animate__bounce">
+    <div className="Content text-center d-flex align-items-center justify-content-center mt-5 animate__animated animate__fadeIn">
     <div className="text-center">
         <Title size="h1">Inicio</Title>
         <Title size="h2">Página no encontrada</Title>
