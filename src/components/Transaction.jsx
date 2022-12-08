b10d0f62384ab3b2d10953e605771c1bc789d818
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-12-08 14:20:13 -0300

    add: styles table

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -4,18 +4,12 @@ import { handleDate } from "../helper/helper";
 export const Transaction = ({ concept, amount, date, type }) => {
   return (
     <tr>
-    <div className="d-flex justify-content-between">
-      <div className="col-12">
-        <td className="text-secondary">{concept}</td>
-      </div>
-    </div>
+        <td scope="row">{concept}</td>
     <td>
-      <p className="text-secondary">${amount}</p>
+      ${amount}
       </td>
     <td>
-      <p className="text-secondary">
         <span className={`badge ${type === "topup" ? "bg-success" : "bg-danger"}`}>{type}</span>
-      </p>
       </td>
     <td>
       <p className="text-secondary">{handleDate(date)}</p>
