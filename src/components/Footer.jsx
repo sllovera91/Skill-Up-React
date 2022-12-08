796c3937be594baaa4daf3679b4242da547fb9db
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-12-08 11:29:11 -0300

    add: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import icons from "../assets/icons";
 
 export const Footer = () => {
   return (
-    <footer className="footer text-center text-white bg-primary">
+    <footer className="text-center text-lg-start bg-light text-muted">
       <div className="text-center p-4">
-        2022 - Grupo Alkemy
+        <div className="d-flex justify-content-center">
+        <a className="text-decoration-none d-flex justify-content-center mx-4" href="https://www.linkedin.com/in/mariano-pividori-5680a051/" >
+            <img src={icons.linkedin} alt="linkedin" width="16" height="16"/>
+            <h6 className="px-2 text-muted">Mariano Pividori</h6>
+          </a>
+
+          <a className="text-decoration-none d-flex justify-content-center mx-4" href="https://www.linkedin.com/in/santiago-llovera-6841931a2/" >
+            <img src={icons.linkedin} alt="linkedin" width="16" height="16"/>
+            <h6 className="px-2 text-muted">Santiago Llovera</h6>
+          </a>
+          <a className="text-decoration-none d-flex justify-content-center mx-4" href="https://www.linkedin.com/in/santiagobarreto-/" >
+            <img src={icons.linkedin} alt="linkedin" width="16" height="16" />
+            <h6 className="px-2 text-muted">Santiago Barreto</h6>
+          </a>
+        </div>
       </div>
     </footer>
   );
