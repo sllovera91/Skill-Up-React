c924d0a6673b16fff94c3bdda3c30445b3ccd8ad
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-12-11 00:57:09 -0300

    update: pagination control

--- src/components/PaginationControl.jsx
+++ src/components/PaginationControl.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 
-export const PaginationControl = () => {
+export const PaginationControl = ({ page, setPage, nextPage }) => {
   return (
     <nav>
-    <ul className="col justify-content-center pagination ">
-      <li className="page-item ">
-        <a className="page-link" href="#" tabIndex="-1">Anterior</a>
-      </li>
-      <li className="page-item"><a className="page-link" href="#">1</a></li>
-      <li className="page-item active">
-        <a className="page-link" href="#">2 </a>
-      </li>
-      <li className="page-item"><a className="page-link">3</a></li>
-      <li className="page-item">
-        <a className="page-link">Siguente</a>
-      </li>
-    </ul>
-  </nav>
+      <ul className="col justify-content-center pagination ">
+        <li className="page-item">
+          <a className={page !== 1 ? "page-link " : "page-link text-muted disabled"} onClick={() => setPage(page === 0 ? 0 : page - 1)}>Anterior</a>
+        </li>
+        <li className="page-item">
+          <a className={nextPage ? "page-link " : "page-link text-muted disabled"} onClick={() => setPage(page + 1)}>Siguente</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
